e4dafe83260700267fb6a9316decb674aaeebe4d
Nicholas de Jong <me@nicholasdejong.com>
2020-12-15 16:09:38 +1000

    package update

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## 0.3.1 (2020-12-15)
+
+* __general__
+    * **feature**: Rearrange project structure
+
--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ requirements = [
 
 setuptools.setup(
   name = 'arpwitch',
-  version = '0.3.0',
+  version = '0.3.1',
   author = 'Nicholas de Jong',
   author_email = 'contact@nicholasdejong.com',
   description = 'A modern arpwatch replacement with JSON formatted outputs and easy options to exec commands when network changes are observed.',
