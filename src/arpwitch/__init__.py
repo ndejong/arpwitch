f5a4bacb51885dced4372553d92f52c9d150f5cd
Nicholas de Jong <me@nicholasdejong.com>
2020-12-15 16:10:36 +1000

    package update on branch

--- src/arpwitch/__init__.py
+++ src/arpwitch/__init__.py
@@ -3,7 +3,7 @@
 
 __title__ = "arpwitch"
 __author__ = "Nicholas de Jong <contact@nicholasdejong.com>"
-__version__ = '0.3.1'
+__version__ = '0.3.2'
 __license__ = "BSD2"
 
 __logger_default_level__ = 'info'
