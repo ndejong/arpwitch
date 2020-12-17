c15d996a21903ad1be2d8dd5b80fa8a7ccfe262e
Nicholas de Jong <me@nicholasdejong.com>
2020-12-17 11:55:09 +1000

    (arpwitch) bump version to 0.3.5

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ requirements = [
 
 setuptools.setup(
   name = 'arpwitch',
-  version = '0.3.4',
+  version = '0.3.5',
   author = 'Nicholas de Jong',
   author_email = 'contact@nicholasdejong.com',
   description = 'A modern arpwatch replacement with JSON formatted outputs and easy options to exec commands when network changes are observed.',
